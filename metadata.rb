71a8dc05c356d7868f298dd734946ee9693888cd
Peter Donald <peter@realityforge.org>
2013-01-07 11:39:35 +1100

    Add in basic metadata for a cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'hosts'
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
+license          'Apache 2.0'
+description      'Configures the /etc/hosts file'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
