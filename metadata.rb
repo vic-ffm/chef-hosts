4938334df469e5912c7c23a003f734d55319391d
Peter Donald <peter@realityforge.org>
2013-01-07 11:40:53 +1100

    Document the recipe

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'Apache 2.0'
 description      'Configures the /etc/hosts file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
+
+recipe 'hosts::default', 'Configures the /etc/hosts file'
