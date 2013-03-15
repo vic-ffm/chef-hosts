f2a5cecaeceb26661ba54a233e119694c13f4274
Peter Donald <peter@realityforge.org>
2013-03-15 11:22:36 +1100

    Ensure that all host/domain names are in lower case

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,20 @@
+service 'hostname' do
+  supports :start => true
+  action :nothing
+end
+
+ohai 'reload_hosts' do
+  plugin 'network'
+  action :nothing
+end
+
 template '/etc/hosts' do
   source 'hosts.erb'
   owner 'root'
   group 'root'
   mode 0644
-end
-
-service 'hostname' do
-  supports :start => true
-  action :nothing
+  notifies :start, 'service[hostname]', :immediately
+  notifies :reload, 'ohai[reload_hosts]', :immediately
 end
 
 template '/etc/hostname' do
@@ -16,4 +23,5 @@ template '/etc/hostname' do
   group 'root'
   mode 0644
   notifies :start, 'service[hostname]', :immediately
+  notifies :reload, 'ohai[reload_hosts]', :immediately
 end
